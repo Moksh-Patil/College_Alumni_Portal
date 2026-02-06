b4650416d7333c6376e5414316dc363b6b9fb574
Mokshada Patil <mokshadap329@gmail.com>
2026-02-06 20:08:42 +0530

    Initail commit

---
+++ static/script1.js
@@ -0,0 +1,9 @@
+var typed = new Typed (".multiple-text", {
+    strings : ["Future Innovators","Tech Transformers","Rising Stars",
+        "From Learners to Leaders"],
+    typeSpeed:50,
+    backSpeed:100,
+    backDelay:500,
+    loop:true
+})
+
